6827d81d2b86f9189fb943e6dd6c131058da517a
bpoc-mark <markariel.maata@bpoc.co.jp>
2024-02-19 11:42:50 +0800

    change layout from px to vw sec1, cta, contact, footer

--- contact/includes/footer.php
+++ contact/includes/footer.php
@@ -1,7 +1,17 @@
 <?php if (!defined("DOCROOT")) header("location: /"); ?>
 
 <footer class="bg_ft">
-  <div>
-    <p>Copyright © 2023 VALED All Rights Reserved.</p>
+  <div class="mar-style">
+    <div class="footer--container">
+      <div class="footer--des">
+        <p class="p1">© GlobalCast Co.,Ltd. All Rights Reserved. </p>
+        <div class="info--container">
+          <div class="info">
+            <p class="info--p"><a href="https://global-cast.co.jp/privacypolicy/" target="_blank" style="color: #fff;">運営会社</a></p>
+            <p class="info--p1"><a href="https://global-cast.co.jp/" target="_blank" style="color: #fff;">プライバシー・ポリシー</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </footer>
